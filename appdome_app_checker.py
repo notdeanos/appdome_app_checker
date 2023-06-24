48abb07912e58798db4d4006e4968e875856cfeb
Dean Mcdonald <deanos@gmail.com>
2023-06-24 13:40:19 +1000

    Fixed several bugs

--- appdome_app_checker.py
+++ appdome_app_checker.py
@@ -26,12 +26,16 @@ Usage: python appdome_app_checker.py [APP_FILE]
 Note: Ensure that the required dependencies (frida-ios-dump, jadx, unzip, plutil, aapt, strings) are installed.
 
 """
+# Delta
+
+
 import subprocess
 import json
 import sys
 import os
 from androguard.core.bytecodes.apk import APK
-from subprocess import DEVNULL
+from androguard.core.bytecodes.dvm import DalvikVMFormat
+from androguard.core.analysis.analysis import Analysis
 
 # Check if a binary is available in the system PATH
 def is_binary_available(binary):
@@ -41,7 +45,6 @@ def is_binary_available(binary):
     except subprocess.CalledProcessError:
         return False
 
-
 # Install missing binaries/packages using brew
 def install_missing_binaries(missing_binaries):
     for binary in missing_binaries:
@@ -49,10 +52,10 @@ def install_missing_binaries(missing_binaries):
             print(f"Installing {binary}...")
             subprocess.call(['brew', 'install', binary])
 
-
 # Check for obfuscation and perform necessary checks based on the file extension
 def check_for_obfuscation(file_path, file_extension):
     # Check if all required binaries are available
+    #sys.stderr = open(os.devnull, 'w')
     required_binaries = ['frida-ios-dump', 'jadx', 'unzip', 'plutil', 'strings']
     for binary in required_binaries:
         if not is_binary_available(binary):
@@ -63,9 +66,11 @@ def check_for_obfuscation(file_path, file_extension):
     temp_dir = 'temp'
     os.makedirs(temp_dir, exist_ok=True)
 
-    # Extract the app files based on the file extension
+    # Assign app_binary based on file extension
+    app_binary = file_path if file_extension in ['.apk', '.aab'] else None
+
+    # Extract the app
     if file_extension == '.ipa':  # iOS app
-        subprocess.call(['unzip', '-qq', '-o', file_path, '-d', temp_dir], stdout=DEVNULL, stderr=DEVNULL)
         app_binary = subprocess.check_output(['find', temp_dir, '-name', '*.app']).decode().strip()
         check_ios_permissions(app_binary)
         check_debuggable_ios(app_binary)
@@ -74,13 +79,6 @@ def check_for_obfuscation(file_path, file_extension):
         check_ssl_pinning_ios(app_binary)
         check_anti_tampering_protection_ios(app_binary)
     elif file_extension == '.apk' or file_extension == '.aab':  # Android app or app bundle
-        subprocess.call(['unzip', '-o', file_path, '-d', temp_dir])
-        apk_files = [f for f in os.listdir(temp_dir) if f.endswith('.apk')]
-        if apk_files:
-            app_binary = os.path.abspath(os.path.join(temp_dir, apk_files[0]))
-        else:
-            print("No APK file found in the extracted directory.")
-            return
         check_android_permissions(app_binary)
         check_debuggable_android(app_binary)
         check_root_detection_android(app_binary)
@@ -90,11 +88,12 @@ def check_for_obfuscation(file_path, file_extension):
         check_magisk_detection(app_binary)
         check_zygisk_detection_android(app_binary)
     else:
-        print("Unsupported file format.")
+        print(f"Unsupported file format. File extension received: {file_extension}")
         return
 
     # Cleanup temporary files
     subprocess.call(['rm', '-rf', temp_dir])
+    sys.stderr = sys.__stderr__
 
 # Check app permissions (iOS)
 def check_ios_permissions(app_binary):
@@ -123,17 +122,14 @@ def check_ios_permissions(app_binary):
         except:
             pass
 
-
 # Check app permissions (Android)
 def check_android_permissions(app_binary):
-    with APK(app_binary) as apk:
-        manifest_data = apk.get_manifest_xml()
+    apk = APK(app_binary)
     permissions = apk.get_permissions()
     if permissions:
         print("Permissions:")
         for permission in permissions:
             print("-", permission)
-
 
 # Check if the app is debuggable (iOS)
 def check_debuggable_ios(app_binary):
@@ -146,19 +142,23 @@ def check_debuggable_ios(app_binary):
                 entitlements = plist_data['Entitlements']
                 if 'get-task-allow' in entitlements and entitlements['get-task-allow']:
                     print("The iOS app is debuggable.")
+                else:
+                    print("The iOS app is not debuggable.")
+            else:
+                print("Unable to check debuggable status.")
         except:
-            pass
-
+            print("Unable to check debuggable status.")
 
 # Check if the app is debuggable (Android)
 def check_debuggable_android(app_binary):
     apk = APK(app_binary)
-    with APK(app_binary) as apk:
-        manifest_data = apk.get_manifest_xml()
-    debuggable = apk.get_effective_debug()
-    if debuggable:
-        print("The Android app is debuggable.")
-
+    debuggable = apk.get_element('application', '{http://schemas.android.com/apk/res/android}debuggable')
+    if debuggable == 'true':
+        print("App is debuggable.")
+    elif debuggable == 'false':
+        print("App is not debuggable.")
+    else:
+        print("Unable to check debuggable status.")
 
 # Check for root detection (iOS)
 def check_root_detection_ios(app_binary):
@@ -166,14 +166,30 @@ def check_root_detection_ios(app_binary):
     result_str = result.decode().lower()
     if 'cydia' in result_str or 'jailbreak' in result_str:
         print("The iOS app has signs of root detection.")
-
+    else:
+        print("The iOS app does not have signs of root detection.")
 
 # Check for root detection (Android)
 def check_root_detection_android(app_binary):
-    result = subprocess.check_output(['strings', app_binary])
-    result_str = result.decode().lower()
-    if 'root' in result_str or 'su' in result_str:
-        print("The Android app has signs of root detection.")
+    # India
+    """
+    Check for root detection in the Android app.
+    """
+    apk = APK(app_binary)
+    arsc_parser = apk.get_android_resources()
+    if arsc_parser is not None:
+        strings_set = set()
+        for resource_key in arsc_parser.resource_keys:
+            if resource_key.package_name == 'android' and resource_key.type_name == 'string':
+                resource_value = arsc_parser.get_string(resource_key)
+                if resource_value is not None:
+                    strings_set.add(resource_value)
+        if any('root' in string.lower() or 'su' in string.lower() for string in strings_set):
+            print("The Android app has signs of root detection.")
+        else:
+            print("The Android app does not have signs of root detection.")
+    else:
+        print("Failed to retrieve Android resources.")
 
 
 # Check for Frida detection (iOS)
@@ -182,7 +198,8 @@ def check_frida_detection_ios(app_binary):
     result_str = result.decode().lower()
     if 'frida' in result_str:
         print("The iOS app has signs of Frida detection.")
-
+    else:
+        print("The iOS app does not have signs of Frida detection.")
 
 # Check for Frida detection (Android)
 def check_frida_detection_android(app_binary):
@@ -190,7 +207,8 @@ def check_frida_detection_android(app_binary):
     result_str = result.decode().lower()
     if 'frida' in result_str:
         print("The Android app has signs of Frida detection.")
-
+    else:
+        print("The Android app does not have signs of Frida detection.")
 
 # Check for SSL/TLS pinning (iOS)
 def check_ssl_pinning_ios(app_binary):
@@ -198,7 +216,8 @@ def check_ssl_pinning_ios(app_binary):
     result_str = result.decode().lower()
     if 'sslpinning' in result_str or 'ssl pinning' in result_str:
         print("The iOS app has signs of SSL/TLS pinning.")
-
+    else:
+        print("The iOS app does not have signs of SSL/TLS pinning.")
 
 # Check for SSL/TLS pinning (Android)
 def check_ssl_pinning_android(app_binary):
@@ -206,34 +225,36 @@ def check_ssl_pinning_android(app_binary):
     result_str = result.decode().lower()
     if 'sslpinning' in result_str or 'ssl pinning' in result_str:
         print("The Android app has signs of SSL/TLS pinning.")
-
+    else:
+        print("The Android app does not have signs of SSL/TLS pinning.")
 
 # Check for anti-tampering protection (iOS)
 def check_anti_tampering_protection_ios(app_binary):
-    result = subprocess.check_output(['jadx', '-j', app_binary])
+    result = subprocess.check_output(['jadx', app_binary])
     result_str = result.decode().lower()
     if 'j2waf' in result_str or 'tamper' in result_str:
         print("The iOS app has signs of anti-tampering protection.")
-
+    else:
+        print("The iOS app does not have signs of anti-tampering protection.")
 
 # Check for anti-tampering protection (Android)
 def check_anti_tampering_protection_android(app_binary):
-    result = subprocess.check_output(['jadx', '-j', app_binary])
+    result = subprocess.check_output(['jadx', app_binary])
     result_str = result.decode().lower()
     if 'j2waf' in result_str or 'tamper' in result_str:
         print("The Android app has signs of anti-tampering protection.")
-
+    else:
+        print("The Android app does not have signs of anti-tampering protection.")
 
 # Check for Magisk detection (Android)
 def check_magisk_detection(app_binary):
-    with APK(app_binary) as apk:
-        manifest_data = apk.get_manifest_xml()
+    apk = APK(app_binary)
+    manifest_data = apk.get_android_manifest_xml()
     magisk_detection = 'com.topjohnwu.magisk' in manifest_data
     if magisk_detection:
         print("Magisk Detection: Detected")
     else:
         print("Magisk Detection: Not Detected")
-
 
 # Check for Zygisk detection (Android)
 def check_zygisk_detection_android(app_binary):
@@ -241,8 +262,8 @@ def check_zygisk_detection_android(app_binary):
     result_str = result.decode().lower()
     if 'zygisk' in result_str:
         print("The Android app has signs of Zygisk detection.")
-
-
+    else:
+        print("The Android app does not have signs of Zygisk detection.")
 
 # Main function
 def main():
@@ -263,6 +284,4 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
 
